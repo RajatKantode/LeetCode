94b9224fa46f67fe8dde01ac65a7aac75e490111
Rajat Kantode <rajatskantode2@gmail.com>
2026-07-16 19:08:48 +0530

    Time: 4 ms (41.51%) | Memory: 12.3 MB (89.89%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -5,8 +5,6 @@ class Solution(object):
         :rtype: List[int]
         """
         nums.sort()
-        l=[]
         for i in range(0,len(nums),2):
-            l.append(nums[i+1])
-            l.append(nums[i])
-        return l
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
