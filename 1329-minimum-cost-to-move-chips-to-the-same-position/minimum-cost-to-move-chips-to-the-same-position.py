5aa591a0fc21ac3b7f27383beb7eedefab24df72
Rajat Kantode <rajatskantode2@gmail.com>
2026-08-28 19:31:10 +0530

    Time: 1 ms (7.78%) | Memory: 12.5 MB (0.00%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minCostToMoveChips(self, position):
+        """
+        :type position: List[int]
+        :rtype: int
+        """
+        even=0
+        odd=0
+
+        for i in position:
+            if i%2==0:
+                even+=1
+            else:
+                odd+=1
+
+        return min(even,odd)
