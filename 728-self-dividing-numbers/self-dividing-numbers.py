5066b790267cf2dc5f18264a25e755ec2e6bc90c
Rajat Kantode <rajatskantode2@gmail.com>
2026-09-16 19:22:23 +0530

    Time: 7 ms (69.75%) | Memory: 12.6 MB (15.13%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        result = []
+
+        for num in range(left, right + 1):
+
+            temp = num
+            valid = True
+
+            while temp > 0:
+                digit = temp % 10
+
+                if digit == 0 or num % digit != 0:
+                    valid = False
+                    break
+
+                temp //= 10
+
+            if valid:
+                result.append(num)
+
+        return result
+        
